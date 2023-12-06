62cfb27889bf3e0d1385a07915eb385fc0c8e829
Lisa Petersson <lisasdezign@gmail.com>
2023-12-06 10:10:53 +0100

    css

--- src/components/HelpContact/HelpContact.jsx
+++ src/components/HelpContact/HelpContact.jsx
@@ -9,19 +9,32 @@ export default function HelpContact() {
   return (
     <section>
 
-        <div className={styles.FlowLeft}> 
-            <div>
-                <h1>Hjälp & kontakt</h1>
-                <p>Här hittar du hjälp och vanligt ställda frågor.
-                    Hittar du fel eller buggar på hemsidan får du gärna berätta det för oss på <a href="mailto:mailadress@email.se"> mailadress@email.se</a> </p>
-            </div>
+            <div className={styles.flowLeft}> 
+                <article>
+                    <h1>Hjälp & kontakt</h1>
+                    <p>Här hittar du hjälp och vanligt ställda frågor.
+                        Hittar du fel eller buggar på hemsidan får du gärna berätta det för oss på <a href="mailto:mailadress@email.se"> mailadress@email.se</a> </p>
+                </article>
 
-            <div>
-                <h2>Vanliga frågor</h2>
+                <article className={styles.faq}>
+                    <h2>Vanliga frågor</h2>
 
-                <Accordion flush>
+                    <Accordion className={styles.accordion}>
 
-                    <Accordion.Item eventKey="0">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Detta är en vanlig fråga?</Accordion.Header>
+                                <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                culpa qui officia deserunt mollit anim id est laborum.
+                                </Accordion.Body>
+                        </Accordion.Item>
+
+                    <Accordion.Item eventKey="1">
                         <Accordion.Header>Detta är en vanlig fråga?</Accordion.Header>
                             <Accordion.Body>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -32,67 +45,54 @@ export default function HelpContact() {
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                             culpa qui officia deserunt mollit anim id est laborum.
                             </Accordion.Body>
-                </Accordion.Item>
+                    </Accordion.Item>
 
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Detta är en vanlig fråga?</Accordion.Header>
-                        <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                </Accordion.Item>
+                    <Accordion.Item eventKey="2">
+                        <Accordion.Header>Detta är en vanlig fråga?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                    </Accordion.Item>
 
-                <Accordion.Item eventKey="2">
-                    <Accordion.Header>Detta är en vanlig fråga?</Accordion.Header>
-                        <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                </Accordion.Item>
+                    <Accordion.Item eventKey="3">
+                        <Accordion.Header>Detta är en vanlig fråga?</Accordion.Header>
+                            <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                    </Accordion.Item>
 
-                <Accordion.Item eventKey="3">
-                    <Accordion.Header>Detta är en vanlig fråga?</Accordion.Header>
-                        <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                </Accordion.Item>
+                </Accordion>
+                </article>
 
-            </Accordion>
+                <article className={styles.contact}>
+                    <h3>Kontakt</h3>
+                    <p>Behöver du mer hjälp eller undrar du över något når du oss på <a href="mailto:mailadress@email.se"> mailadress@email.se</a></p>
+                </article>
+            </div>
+
+            <div className={styles.flowRight}>
+                <div>
+                    <p>Placeholder för ikon av avatar</p>
+                    <p>Placeholder för ikon av avatar</p>
+                    <p>Placeholder för ikon av avatar</p>
+                </div>
             </div>
 
             <div>
-                <h3>Kontakt</h3>
-                <p>Behöver du mer hjälp eller undrar du över något når du oss på <a href="mailto:mailadress@email.se"> mailadress@email.se</a></p>
+                <CloseButton /> 
             </div>
-        </div>
-
-        <div className={styles.FlowRight}>
-            <div>
-                <p>Placeholder för ikon av avatar</p>
-                <p>Placeholder för ikon av avatar</p>
-                <p>Placeholder för ikon av avatar</p>
-            </div>
-        </div>
-
-        <div>
-            <CloseButton /> 
-        </div>
     </section>
   );
 }
