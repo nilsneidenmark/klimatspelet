18ee33171c3310b5816fa7efac01c6638f1f1f54
Typhann <72917604+Typhann@users.noreply.github.com>
2023-12-06 13:04:58 +0100

    fix header and footer width

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,14 @@ import "../Footer/footer.module.scss";
 export default function Footer() {
   return (
     <footer>
-      <h2>EcoExplorer</h2>
-      <ul>
-        <li>Hjälp & kontakt</li>
-        <li>Behandling av personuppgifter</li>
-        <li>Hantering av kakor</li>
-      </ul>
+      <div>
+        <h2>EcoExplorer</h2>
+        <ul>
+          <li>Hjälp & kontakt</li>
+          <li>Behandling av personuppgifter</li>
+          <li>Hantering av kakor</li>
+        </ul>
+      </div>
     </footer>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,12 @@ import "../Header/header.module.scss";
 export default function Header() {
   return (
     <header>
-      <h2>EcoExplorer</h2>
       <div>
-        <button>Skapa konto</button>
-        <button className="primarybtn">Logga in</button>
+        <h2>EcoExplorer</h2>
+        <div>
+          <button>Skapa konto</button>
+          <button className="primarybtn">Logga in</button>
+        </div>
       </div>
     </header>
   );
