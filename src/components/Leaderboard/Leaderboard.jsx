15c5a55fa335d9305d184599a2814a3ca64833be
Lisa Petersson <lisasdezign@gmail.com>
2023-12-08 08:13:32 +0100

    .

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,9 +1,17 @@
-
-
-
 export default function Leaderboard() {
     <section>
-
-
+        <div>
+            <h2>Min placering</h2>
+            <p>5</p>
+        </div>
+        
+        <div>
+            <h2>Topplista</h2>
+            <ol>
+                <li>Namn</li>
+                <li>Namn</li>
+                <li>Namn</li>
+            </ol>
+        </div>
     </section>
 }
--- src/routes/Profile/Profile.jsx
+++ src/routes/Profile/Profile.jsx
@@ -1,5 +1,15 @@
 // Renderar Profilsidan för elever
-import styles from "./profile.module.scss";
-export default function Home() {
-  return <h1 className={styles.red}>Profile</h1>;
+import Categories from "../../components/Categories/Categories";
+import Leaderboard from "../../components/Leaderboard/Leaderboard";
+// import Services from "../../components/Services/Services";
+import About from "../../components/About/About";
+
+export default function Profile() {
+  return (
+    <>
+      <Categories />
+      <Leaderboard />
+      <About />
+    </>
+  );
 }
