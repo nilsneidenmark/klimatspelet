b2ec1b5089706f8800bd9ea6764a6387e12769c1
Typhann <72917604+Typhann@users.noreply.github.com>
2023-12-08 12:46:44 +0100

    add test buttons

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -7,6 +7,24 @@ import Settings from "../../components/Settings/Settings";
 export default function Home() {
   return (
     <>
+      <section className="test-section">
+        <h2>Primary</h2>
+        <div className="primary-buttons">
+          <button className="opt1">Test</button>
+          <button className="opt">Tests</button>
+          <button className="opt3">Test</button>
+          <button className="opt4">Test</button>
+          <button className="opt5">Test</button>
+        </div>
+        <h2>Secondary</h2>
+        <div className="secondary-buttons">
+          <button className="opt1">Test</button>
+          <button className="opt2">Test</button>
+          <button className="opt3">Test</button>
+          <button className="opt4">Test</button>
+          <button className="opt5">Test</button>
+        </div>
+      </section>
       <Settings />
       <Login />
       <Intro />
