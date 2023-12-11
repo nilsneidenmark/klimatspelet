d6f0c0068472a6a8e05d1959294fa8976202f7f1
Typhann <72917604+Typhann@users.noreply.github.com>
2023-12-11 17:43:34 +0100

    add avatar menu

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,26 @@
+import avatarBear from "../../assets/avatar_bear.webp";
+import avatarDog from "../../assets/avatar_dog.webp";
+import avatarGreen from "../../assets/avatar_gamer_green.webp";
+import avatarPink from "../../assets/avatar_gamer_pink.webp";
+import avatarGiraffe from "../../assets/avatar_giraffe.webp";
+import avatarGorilla from "../../assets/avatar_gorilla.webp";
+import avatarRabbit from "../../assets/avatar_rabbit.webp";
+import avatarWalrus from "../../assets/avatar_walrus.webp";
+import arrow from "../../assets/icons/chevron-down.svg";
+import styles from "./avatar.module.scss";
+
+export default function Avatar() {
+  return (
+    <div tabIndex={0} className={styles.avatar}>
+      <div className={styles.points}>
+        <p className={styles.star}>⭐</p>
+        <p>0</p>
+      </div>
+
+      <img src={avatarBear} alt="avatar" />
+      <div className={styles.arrow}>
+        <img src={arrow} alt="down arrow" />
+      </div>
+    </div>
+  );
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import "../Header/header.module.scss";
+import Avatar from "../Avatar/Avatar";
 
 export default function Header() {
   return (
     <header>
       <div>
         <h2>EcoExplorer</h2>
+        {/* <Avatar /> */}
         <div>
           <button>Skapa konto</button>
           <button className="primarybtn">Logga in</button>
