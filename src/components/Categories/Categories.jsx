046c5aed722067aa3f14e62cec5f7dbc1082070c
Typhann <72917604+Typhann@users.noreply.github.com>
2023-12-13 18:25:44 +0100

    change scrollbar

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -4,58 +4,55 @@ import pineTree from "../../assets/category_pine-tree.webp";
 import buildings from "../../assets/category_buildings.webp";
 import glacier from "../../assets/category_glacier.webp";
 import desert from "../../assets/category_desert.webp";
+import { useNavigate } from "react-router-dom";
 
 export default function Categories() {
-    return (
-        <section>
-            <h1>Kategorier</h1>
-            <div className={styles.categoriesSection}>
+  const navigate = useNavigate();
+  return (
+    <section>
+      <h1>Kategorier</h1>
+      <div className={styles.categoriesSection}>
+        <button className={styles.categories}>
+          <img src={ocean} />
+          <p>Världens hav</p>
+          <small>0/12</small>
+        </button>
 
-                <div className={styles.categories}>
-                    <img src={ocean} />
-                    <p>Världens hav</p>
-                    <small>2/12</small>
-                </div>
+        <button disabled className={styles.categories}>
+          <img src={ocean} />
+          <p>Energi</p>
+          <p className={styles.comingSoon}>Kommer snart</p>
+          <small>0/12</small>
+        </button>
 
-                <div className={styles.categories}>
-                    <img src={ocean} />
-                    <p>Energi</p>
-                    <p className={styles.comingSoon}>Kommer snart</p>
-                    <small>2/12</small>
-                </div>
+        <button disabled className={styles.categories}>
+          <img src={pineTree} />
+          <p>Skogen</p>
+          <p className={styles.comingSoon}>Kommer snart</p>
+          <small>0/12</small>
+        </button>
 
-                <div className={styles.categories}>
-                    <img src={pineTree} />
-                    <p>Skogen</p>
-                    <p className={styles.comingSoon}>Kommer snart</p>
-                    <small>2/12</small>
-                </div>
-            </div>
+        <button disabled className={styles.categories}>
+          <img src={buildings} />
+          <p>Människan</p>
+          <p className={styles.comingSoon}>Kommer snart</p>
+          <small>0/12</small>
+        </button>
 
-            <div className={styles.categoriesSection}>
+        <button disabled className={styles.categories}>
+          <img src={glacier} />
+          <p>Glaciärer</p>
+          <p className={styles.comingSoon}>Kommer snart</p>
+          <small>0/12</small>
+        </button>
 
-            <div className={styles.categories}>
-                    <img src={buildings} />
-                    <p>Människan</p>
-                    <p className={styles.comingSoon}>Kommer snart</p>
-                    <small>2/12</small>
-                </div>
-
-                <div className={styles.categories}>
-                    <img src={glacier} />
-                    <p>Glaciärer</p>
-                    <p className={styles.comingSoon}>Kommer snart</p>
-                    <small>2/12</small>
-                </div>
-
-                <div className={styles.categories}>
-                    <img src={desert} />
-                    <p>Öknen</p>
-                    <p className={styles.comingSoon}>Kommer snart</p>
-                    <small>2/12</small>
-                </div>
-
-            </div>
-        </section>
-    );
+        <button disabled className={styles.categories}>
+          <img src={desert} />
+          <p>Öknen</p>
+          <p className={styles.comingSoon}>Kommer snart</p>
+          <small>0/12</small>
+        </button>
+      </div>
+    </section>
+  );
 }
