b7add908d806926ab4d0f91bf332a0b9b9687aa0
Lisa Petersson <lisasdezign@gmail.com>
2024-01-01 15:34:33 +0100

    cards

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,62 +4,50 @@ export default function Services() {
   return (
     <section>
       <h2>Rekomenderade tjänster</h2>
-      <p>Vill du lära dig mer om klimatförämdtingarna och dess påverkan? 
+      <p>Vill du lära dig mer om klimatförändringarna och dess påverkan? 
         Klicka då på rutorna nedanför för att komma till andra sidor med, spel, tester och spännande fakta.</p>
       <div className={styles.services}>
 
-         <div className={styles.card}>
+         <div className={`${styles.card} ${styles.cardOne}`}>
             <a href="https://www.example.com"> 
-              <div className={styles.cardOne}>
                 <h3>Klimatkalkylatorn</h3>
                 <p>Du kan beräkna din miljöpåverkan här</p>
-              </div>
             </a>
          </div>
 
-         <div className={styles.card}>
+         <div className={`${styles.card} ${styles.cardTwo}`}>
             <a href="https://www.example.com"> 
-              <div className={styles.cardTwo}>
                 <h3>NASA ClimateKids</h3>
                 <p>Lär dig mer om klimatet genom spel på NASA ClimateKids.</p>
-              </div>
             </a>
-          </div>
-
-         <div className={styles.card}>
-          <a href="https://www.example.com"> 
-            <div className={styles.cardThree}>
-              <h3>Rapport om Golfströmmen</h3>
-              <p>Den havsström som avgör livsvillkoren för oss i Norden håller på att bromsa in. </p>
-            </div>
-          </a>
          </div>
 
-         <div className={styles.card}>
-          <a href="https://www.example.com"> 
-            <div className={styles.cardFour}>
-              <h3>Klimatflyktingar</h3>
-              <p>Många klimatflyktingar har ingenstans att ta vägen när havsnivån blir högre.</p>
-            </div>
-          </a>
+         <div className={`${styles.card} ${styles.cardThree}`}>
+            <a href="https://www.example.com"> 
+                <h3>Rapport om Golfströmmen</h3>
+                <p>Den havsström som avgör livsvillkoren för oss i Norden håller på att bromsa in.</p>
+            </a>
          </div>
 
-        <div className={styles.card}>
-          <a href="https://www.example.com">
-            <div className={styles.cardFive}>
-              <h3>Arktis - isarna smälter</h3>
-              <p>När isarna försvinner blir det varmare och vädret blir mer extremt</p>
-            </div>
-          </a>
-        </div>
+         <div className={`${styles.card} ${styles.cardFour}`}>
+            <a href="https://www.example.com"> 
+                <h3>Klimatflyktingar</h3>
+                <p>Många klimatflyktingar har ingenstans att ta vägen när havsnivån blir högre.</p>
+            </a>
+         </div>
 
-         <div className={styles.card}>
-          <a href="https://www.example.com">
-            <div className={styles.cardSex}>
-              <h3>Sveriges klimatutsläpp</h3>
-              <p>Sveriges klimatutsläpp minskade med fem procent under 2022 </p>
-            </div>
-          </a>
+         <div className={`${styles.card} ${styles.cardFive}`}>
+            <a href="https://www.example.com"> 
+                <h3>Arktis - isarna smälter</h3>
+                <p>När isarna försvinner blir det varmare och vädret blir mer extremt.</p>
+            </a>
+         </div>
+
+         <div className={`${styles.card} ${styles.cardSex}`}>
+            <a href="https://www.example.com"> 
+                <h3>Sveriges klimatutsläpp</h3>
+                <p>Sveriges klimatutsläpp minskade med fem procent under 2022.</p>
+            </a>
          </div>
 
       </div>
