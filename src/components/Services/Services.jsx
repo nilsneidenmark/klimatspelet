7f5850f210a0d648433ddd43a2a428e0f2641571
Lisa Petersson <lisasdezign@gmail.com>
2024-01-01 15:53:07 +0100

    card links

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -9,42 +9,42 @@ export default function Services() {
       <div className={styles.services}>
 
          <div className={`${styles.card} ${styles.cardOne}`}>
-            <a href="https://www.example.com"> 
+            <a className={styles.serviceLink} href="https://www.example.com"> 
                 <h3>Klimatkalkylatorn</h3>
                 <p>Du kan beräkna din miljöpåverkan här</p>
             </a>
          </div>
 
          <div className={`${styles.card} ${styles.cardTwo}`}>
-            <a href="https://www.example.com"> 
+            <a className={styles.serviceLink} href="https://www.example.com"> 
                 <h3>NASA ClimateKids</h3>
                 <p>Lär dig mer om klimatet genom spel på NASA ClimateKids.</p>
             </a>
          </div>
 
          <div className={`${styles.card} ${styles.cardThree}`}>
-            <a href="https://www.example.com"> 
+            <a className={styles.serviceLink} href="https://www.example.com"> 
                 <h3>Rapport om Golfströmmen</h3>
                 <p>Den havsström som avgör livsvillkoren för oss i Norden håller på att bromsa in.</p>
             </a>
          </div>
 
          <div className={`${styles.card} ${styles.cardFour}`}>
-            <a href="https://www.example.com"> 
+            <a className={styles.serviceLink} href="https://www.example.com"> 
                 <h3>Klimatflyktingar</h3>
                 <p>Många klimatflyktingar har ingenstans att ta vägen när havsnivån blir högre.</p>
             </a>
          </div>
 
          <div className={`${styles.card} ${styles.cardFive}`}>
-            <a href="https://www.example.com"> 
+            <a className={styles.serviceLink} href="https://www.example.com"> 
                 <h3>Arktis - isarna smälter</h3>
                 <p>När isarna försvinner blir det varmare och vädret blir mer extremt.</p>
             </a>
          </div>
 
          <div className={`${styles.card} ${styles.cardSex}`}>
-            <a href="https://www.example.com"> 
+            <a className={styles.serviceLink} href="https://www.example.com"> 
                 <h3>Sveriges klimatutsläpp</h3>
                 <p>Sveriges klimatutsläpp minskade med fem procent under 2022.</p>
             </a>
