a97d163be8141d29edc75ee77a4bc9ef292740b9
Lisa Petersson <lisasdezign@gmail.com>
2024-01-02 10:38:34 +0100

    css fix + logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import "../Footer/footer.module.scss";
+import logo from "../../assets/logo/logotyp_minst.png"
 
 export default function Footer() {
   return (
     <footer>
       <div>
-        <h2>EcoExplorer</h2>
+      <img src={logo} alt="logotype for EcoExplorer"/>
         <ul>
           <li>Hjälp & kontakt</li>
           <li>Behandling av personuppgifter</li>
