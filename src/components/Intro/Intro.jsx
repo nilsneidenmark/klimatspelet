14bdaf9094a7c38186d96d57ed1b298a47eecc42
Typhann <72917604+Typhann@users.noreply.github.com>
2024-01-08 10:00:57 +0100

    update image paths

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,6 @@
-import slide1 from "../../assets/slider/Slide1.png";
-import slide2 from "../../assets/slider/Slide2.png";
-import slide3 from "../../assets/slider/Slide3.png";
+import slide1 from "../../assets/slider/slide1.png";
+import slide2 from "../../assets/slider/slide2.png";
+import slide3 from "../../assets/slider/slide3.png";
 import styles from "./intro.module.scss";
 import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
